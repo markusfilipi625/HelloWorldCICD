e6023b83cb6dddf9862c8de4fc0d8c35a1372672
Tribhuvon Tallapragada <tribhuvon.tallapragada@sap.com>
2019-04-01 09:15:48 -0400

    Modified POM FILE

--- src/main/java/com/helloworld/cicd/HelloWorldCicdApplication.java
+++ src/main/java/com/helloworld/cicd/HelloWorldCicdApplication.java
@@ -2,11 +2,12 @@ package com.helloworld.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.helloworld.cicd")
-public class HelloWorldCicdApplication {
+public class HelloWorldCicdApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldCicdApplication.class, args);
