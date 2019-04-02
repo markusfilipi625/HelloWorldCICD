649f33aa5d7bc5e2bd1af9bd6d47bbebb84d5b3d
Tribhuvon Tallapragada <tribhuvon.tallapragada@sap.com>
2019-04-01 22:30:56 -0400

    HelloWorldCicdApplication

--- src/main/java/com/helloworld/cicd/HelloWorldCicdApplication.java
+++ src/main/java/com/helloworld/cicd/HelloWorldCicdApplication.java
@@ -2,15 +2,20 @@ package com.helloworld.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.helloworld.cicd")
-public class HelloWorldCicdApplication extends SpringBootServletInitializer{
+public class HelloWorldCicdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldCicdApplication.class, args);
 	}
 
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HelloWorldCicdApplication.class);
+	}
 }
